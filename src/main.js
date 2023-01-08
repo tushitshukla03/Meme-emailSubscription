df52f27ad8481542bdb8115158cb9410ccd86365
tushitshukla03 <97801384+tushitshukla03@users.noreply.github.com>
2023-01-08 11:22:13 +0530

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+const cron = require("node-cron");
+
+const dotenv = require('dotenv'); 
+
+dotenv.config();
+
+const mailOPtions = {
+  from: process.env.MAIL_USER_EMAIL,
+  to: process.env.MAIL_TO,
+  subject: "Daily Top News..",
+  html: <h1>hello</h1>
+
+}
+
+
+
+const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.MAIL_USER_EMAIL,
+            pass: process.env.MAIL_USER_PASSWORD
+        },
+    });
+
+transporter.sendMail(mailOPtions,(error,info)=>{
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email send:" + info.response);
+        }
+      });   
+
+
+
