c6b6c4a86a103a8b0910727b567a235373dc5526
tushitshukla03 <97801384+tushitshukla03@users.noreply.github.com>
2023-01-31 20:18:57 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
-
-
+const path = require("path");
 const dotenv = require('dotenv'); 
 
 dotenv.config();
 
+
+const htmlFile = path.resolve(__dirname, "./meme.html");
+const html = fs.readFileSync(htmlFile, "utf-8");
+
 const mailOPtions = {
   from: process.env.MAIL_USER_EMAIL,
   to: process.env.MAIL_TO,
-  subject: "Daily Top News..",
-  html: fs.readFileSync("../src/meme.html", "utf-8")
-
+  subject: "Daily Top Memes..",
+  html: html,
 }
-
 
 
 const transporter = nodemailer.createTransport({
