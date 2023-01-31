f6dfcd5a9a1f4ad320f2357c030a0606c1d9a212
tushitshukla03 <97801384+tushitshukla03@users.noreply.github.com>
2023-01-31 18:05:27 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-
+const meme = require("./meme");
 
 
 const dotenv = require('dotenv'); 
@@ -10,7 +10,7 @@ const mailOPtions = {
   from: process.env.MAIL_USER_EMAIL,
   to: process.env.MAIL_TO,
   subject: "Daily Top News..",
-  html: "<h1>hello</h1>"
+  html: meme
 
 }
 
