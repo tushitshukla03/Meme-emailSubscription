68e2ba01d70ec577dcc5b065481f8bdd0f699caf
tushitshukla03 <97801384+tushitshukla03@users.noreply.github.com>
2023-01-31 21:36:15 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,57 +1,44 @@
 const nodemailer = require("nodemailer");
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 const dotenv = require('dotenv'); 
-
 dotenv.config();
 
-const recipients = process.env.MAIL_TO.split(" "); // recipients are stored as a comma-separated string in .env file
+async function main() {
+  const response = await axios.get("https://meme-api.com/gimme/dankmemes");
+  const meme = response.data;
 
-const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.MAIL_USER_EMAIL,
-            pass: process.env.MAIL_USER_PASSWORD
-        },
-    });
+  const mailOptions = {
+    from: process.env.MAIL_USER_EMAIL,
+    to: process.env.MAIL_TO,
+    subject: "Daily Top News..",
+    html: `
+      <div>
+        <h1>Top Meme</h1>
+        <img src="${meme.url}" />
+      </div>
+    `
+  };
 
-const generateEmailHTML = async (meme) => {
-  return `
-  <html>
-    <body>
-      <h1>Top Meme of the Day</h1>
-      <img src="${meme.url}" alt="${meme.title}" />
-      <p>${meme.title}</p>
-    </body>
-  </html>
-  `;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.MAIL_USER_EMAIL,
+      pass: process.env.MAIL_USER_PASSWORD
+    },
+  });
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 }
 
-const sendEmails = async (transporter, recipients, emailHTML) => {
-  for (const recipient of recipients) {
-    const mailOptions = {
-      from: process.env.MAIL_USER_EMAIL,
-      to: recipient,
-      subject: "Daily Top News..",
-      html: emailHTML
-    };
-  
-    await transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-      } else {
-        console.log(`Email sent to ${recipient}: ${info.response}`);
-      }
-    });
-  }
-}
+main();
 
-(async () => {
-  const response = await fetch("https://meme-api.com/gimme/dankmemes");
-  const meme = await response.json();
-  const emailHTML = await generateEmailHTML(meme);
-  await sendEmails(transporter, recipients, emailHTML);
-})();
 
 
 
